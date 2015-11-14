247ccc7d43bf71d8d2f6e6709f49ce212c5737d1
MatR <matr@mac-mini-de-matr.home>
2015-11-14 12:46:06 +0100

    First add

--- SwiftTuto1/ViewController.swift
+++ SwiftTuto1/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var age: UITextField!
+    
+    @IBOutlet weak var poids: UITextField!
+    
+    @IBOutlet weak var taille: UITextField!
 
+    @IBOutlet weak var imc: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +27,47 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func Calcul(sender: AnyObject) {
+        
+        var Age = Int(age.text!)
+        var Poids = Double(poids.text!)
+        var Taille = Double(taille.text!)
+        
+        if(age.text! == ""){
+            Age = 18
+            age.text! == String("18")
+        }
+        
+        if(poids.text! == ""){
+            Poids = 70
+            poids.text! == String("70")
+        }
+        
+        if(taille.text! == ""){
+            Taille = 180
+            taille.text! == String("180")
+        }
+        
+        let Calcul0 = Taille! / 100
+        let Calcul1 = Calcul0 * Calcul0
+        let Calcul2 = Poids! / Calcul1
+       
+        if (Age<=30){
+            imc.text = String(Calcul2)
+        }
+        if (Age>=30){
+            let Calcul3 = Calcul2 - 2
+            imc.text = String(Calcul3)
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
 
 
 }
